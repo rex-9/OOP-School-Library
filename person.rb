8ade132b6e276357f1ab6d74b06c9c39b2849945
Htet Naing <htetnaing0814@gmail.com>
2022-09-28 20:18:46 +0630

    fixed linters

--- person.rb
+++ person.rb
@@ -1,21 +1,21 @@
 class Person
-  def initialize(name = 'Unknown', age, parent_permission: true)
-    @id
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Randon.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  attr_reader :id
-  attr_accessor :name, :age
+  private :of_age?
 
-  private :is_of_age?
-
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 end
